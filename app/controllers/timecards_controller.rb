ce9d519a9383518e5282341bc99092825bed6f6f
Rene Duquesnoy <rduquesnoy@drwholdings.com>
2018-06-07 15:55:33 -0500

    CRUD on timecards. After destroy, go back to user

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'date'
 
 class TimecardsController < ApplicationController
-  before_action :set_timecard, only: [:show, :update, :finish]
+  before_action :set_timecard, only: [:show, :update, :finish, :destroy]
 
   def index
     @timecards = Timecard.all
@@ -14,29 +14,26 @@ class TimecardsController < ApplicationController
   end
 
   def update
-    binding.pry
-    @timecard.update_column(:hours, params[:hours])
+		@timecard.update_column(:hours, params['timecard']['hours'])
+		render :show
   end
 
   def edit
     @timecard = Timecard.find(params[:id])
-    binding.pry
   end
 
-  def delete
-    @timecard.destroy
-    binding.pry
-    #Timecard.destroy(params[:id])
+  def destroy
+		user_id=@timecard.user.id
+		@timecard.destroy
+		redirect_to user_path user_id
   end
 
   def start
-    #binding.pry
     @timecard = Timecard.create({user_id: params[:id], start_time: Time.now(), hours: 0 })
     redirect_to main_index_path 
   end
 
   def finish
-    binding.pry
     @timecard.update(hours: ((Time.now - @timecard.start_time)/1.hours).round(2))
     redirect_to main_index_path 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'main#index'
   resources :main, only: [:index, :show]
   resources :users, only: [:index, :show, :edit]
-	resources :timecards, only: [:show, :edit, :delete]
+	resources :timecards #, only: [:show, :edit, :delete]
   get '/', to: 'main#index'
   post '/timecards/start'  => 'timecards#start', as: :timecards_start
   post '/timecards/finish' => 'timecards#finish', as: :timecards_finish
