aefd29006a3d73d292fe68197087dfcdab204e90
rduquesnoy <rduquesnoy@drwholdings.com>
2018-09-14 15:23:43 -0500

    added FY total in top bar

--- app/services/display_timecards.rb
+++ app/services/display_timecards.rb
@@ -10,24 +10,24 @@ class DisplayTimecards
       fy += 1.year
     end
 
-		data = { "user" => @user }
-		year_total=0
-		
-		b = fy
-		12.times do
-			e = b.end_of_month
-    	mon_total = 0.0
-			
-			for ts in @user.timecards.where(start_time: b..e)
-				mon_total += ts.hours
-			end
-			year_total += mon_total
+    data = { "user" => @user }
+    year_total=0
+    
+    b = fy
+    12.times do
+      e = b.end_of_month
+      mon_total = 0.0
+      
+      for ts in @user.timecards.where(start_time: b..e)
+              mon_total += ts.hours
+      end
+      year_total += mon_total
 
-			data[b.strftime("%b").downcase()] = mon_total
+      data[b.strftime("%b").downcase()] = mon_total
 
-			b+=1.month
-		end
-		data['fy'] = year_total
-		data
+      b+=1.month
+    end
+    data['fy'] = year_total
+    data
   end
 end
